7fb5445335eb30f6516ba98e3477c40fb00e455b
Shashank Sharma <rgbkingop7581@gmail.com>
2025-09-18 15:27:17 +0530

    done

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import IdCard from "./idCard/IdCard";
+export default function App() {
+  return <>
+    <IdCard firstName="Shashank" lastName="Sharma" userId="000000000000" batch="Frontend Devlopment" batchId="14" date="07/03/2030" profilePicture="/images/mypic.jpg" />
+  </>
+}
